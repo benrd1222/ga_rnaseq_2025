72b946d6a6204c1031dd202519d69928d0bb96a8
Benjamin Daniel <benjaminclaydaniel@gmail.com>
2026-03-03 14:53:11 -0500

    formatting

--- analysis.r
+++ analysis.r
@@ -9,6 +9,7 @@ library(circlize)
 library(gridtext)
 library(clusterProfiler)
 library(BiocParallel)
+library(writexl)
 #library(VennDiagram)
 
 dat <- read_delim("./data/gaston_counts.txt")
@@ -27,7 +28,7 @@ meta <- as.data.frame(na.omit(meta))
 rownames(meta) <- meta[, 1]
 
 # if we are going to do this all with mgi need to ensure there are no duplicates
-counts <- dat[,-c(1,2,4)]
+counts <- dat[, -c(1, 2, 4)]
 
 counts <- counts %>%
   group_by(external_gene_name) %>%
@@ -38,8 +39,8 @@ sum(duplicated(counts$external_gene_name))
 
 counts <- as.data.frame(counts)
 rownames(counts) <- counts$external_gene_name
-counts <- counts[,-1]
-  
+counts <- counts[, -1]
+
 #filter colnames to match metadata
 colnames(counts) <- sub("^.*?-", "", colnames(counts))
 
@@ -83,25 +84,41 @@ all_differences # should be 3,13,15,24 which all need to be removed from the met
 
 meta <- meta[!rownames(meta) %in% all_differences, ] # removes extra metadata for non-existant samples
 
-meta_grouped <- meta 
-meta_grouped$treatment <- paste(meta_grouped$group,meta_grouped$drug, sep = "_")  
+meta_grouped <- meta
+meta_grouped$treatment <- base::as.factor(paste(
+  meta_grouped$group,
+  meta_grouped$drug,
+  sep = "_"
+))
 
-meta_grouped <- meta_grouped |> 
-  dplyr::filter(drug != "47") |> 
+meta_grouped <- meta_grouped |>
+  dplyr::filter(drug != "47") |>
   dplyr::select(treatment)
 
-counts <- counts[,colnames(counts) %in% rownames(meta_grouped)]
+counts <- counts[, colnames(counts) %in% rownames(meta_grouped)]
 
 counts <- as.matrix(counts)
-meta_grouped <- as.matrix(meta_grouped)
 
-# data organization ----
+meta_grouped_con <- meta_grouped
+meta_grouped_cis <- meta_grouped
+
+meta_grouped_con$treatment <- relevel(
+  meta_grouped$treatment,
+  ref = "CONTROL_Vehicle"
+)
+meta_grouped_cis$treatment <- relevel(
+  meta_grouped$treatment,
+  "CISPLATIN_Vehicle"
+)
+
+# directory organization ----
 
 directories <- c(
   "./results",
   "./results/data",
   "./results/volcano",
-  "./results/GSEA"
+  "./results/GSEA",
+  "./results/oras"
 )
 
 if (all(dir.exists(directories)) != TRUE) {
@@ -118,60 +135,38 @@ if (all(dir.exists(directories)) != TRUE) {
 ## ideally without copying a bunch of code
 # Run DESeq2 ------
 
-dds  <-  DESeqDataSetFromMatrix(countData=counts,
-                                colData=meta_grouped,
-                                design= ~ treatment)
+dds_con <- DESeqDataSetFromMatrix(
+  countData = counts,
+  colData = meta_grouped_con,
+  design = ~treatment
+)
 smallestGroupSize <- 4
-dds <- dds[rowSums(counts(dds) >= 10) >= smallestGroupSize, ]
+dds_con <- dds_con[rowSums(counts(dds_con) >= 10) >= smallestGroupSize, ]
 
-dds <- DESeq(dds)
+dds_con <- DESeq(dds_con)
 
-saveRDS(dds,"./results/dds.rds")
+saveRDS(dds_con, "./results/dds_con.rds")
 
-# dds <- readRDS("./results/dds.rds")
-
-# Extracting results ----
-
-results_dds_names <- resultsNames(dds)[-(resultsNames(dds) == "Intercept")]
-
-res_list <- map(results_dds_names, ~ results(dds, name = .x))
-
-names(res_list) <- results_dds_names
-
-genes_annot <- dat |> 
-  dplyr::select(external_gene_name,description) |> 
-  distinct(external_gene_name, .keep_all = TRUE)
-
-genes_annot <- as.data.frame(genes_annot)
-
-rownames(genes_annot) <- genes_annot$external_gene_name
-genes_annot <- genes_annot[,-1, drop = FALSE]
-
-# raw
-paths <- paste0("./results/data/", results_dds_names, ".csv")
-tmp <- map(
-  res_list,
-  ~ merge(as.data.frame(.x), genes_annot, by = 'row.names', all = FALSE)
+# with cisplatin as reference level
+dds_cis <- DESeqDataSetFromMatrix(
+  countData = counts,
+  colData = meta_grouped_cis,
+  design = ~treatment
 )
-walk2(tmp, paths, write.csv)
+dds_cis <- dds_cis[rowSums(counts(dds_cis) >= 10) >= smallestGroupSize, ]
 
-#filtered
-res_list_filtered <- map(res_list, ~ .x[.x$padj < 0.05 & !is.na(.x$padj), ])
-tmp <- map(
-  res_list_filtered,
-  ~ merge(as.data.frame(.x), genes_annot, by = 'row.names', all = FALSE)
-)
+dds_cis <- DESeq(dds_cis)
 
-paths_filtered <- paste0(
-  "./results/data/",
-  results_dds_names,
-  "_filtered_padj05.csv"
-)
+saveRDS(dds_cis, "./results/dds_cis.rds")
 
-walk2(tmp, paths_filtered, write.csv)
+# Need to essentially run everything below twice...
+list_dds <- list(dds_con, dds_cis)
 
+#TODO:
+# - filter out muscle related genes from the overall pool if possible
+# - Add ORAs to the main analysis
 
-# summary of filtered
+### functions
 
 summarize_dds_res <- function(x, y) {
   sink(sprintf("results/data/%s_filtered_summary.txt", y))
@@ -179,37 +174,9 @@ summarize_dds_res <- function(x, y) {
   sink()
 }
 
-walk2(res_list_filtered, results_dds_names, summarize_dds_res)
-
-# Making the results into filtered, ordered, and annotated dataframes
-cutoff <- 1
-res_workable <- list()
-for (i in 1:length(res_list_filtered)) {
-  tmp <- as.data.frame(res_list_filtered[[i]])
-  tmp <- merge(tmp, genes_annot, by = 'row.names', all = FALSE)
-  
-  tmp <- tmp |>
-    mutate(
-      dir = case_when(
-        log2FoldChange > cutoff ~ "UP",
-        log2FoldChange < -1 * cutoff ~ "DOWN",
-        T ~ "NS"
-      )
-    ) |>
-    arrange(desc(log2FoldChange))
-  
-  res_workable[[i]] <- tmp
-}
-
-names(res_workable) <- results_dds_names
-
-#Volcano Plots:
-
-volcano_annot <- genes_annot[, -2, drop = FALSE]
-
 volcano_dds <- function(x, y, ...) {
   tmp = merge(as.data.frame(x), volcano_annot, by = 'row.names', all = FALSE)
-  
+
   png(
     sprintf("results/Volcano/%s_.png", y),
     width = 800,
@@ -227,85 +194,20 @@ volcano_dds <- function(x, y, ...) {
   dev.off()
 }
 
-walk2(res_list, results_dds_names, ~ volcano_dds(.x, .y, volcano_annot))
-
-# Clustering -------
-
-# for PCAs we need normalized counts and not LFC
-rld_all <- rlog(dds, blind = FALSE)
-rld_all_df <- as.data.frame(assay(rld_all))
-
-intgroup = "treatment" 
-
-PCA_plot_all <- DESeq2::plotPCA(
-  rld_all,
-  intgroup = intgroup,
-  returnData = TRUE,
-  ntop = 1000
-)
-percentVar <- round(100 * attr(PCA_plot_all, "percentVar"))
-
-PCA <- ggplot(
-  PCA_plot_all,
-  aes_string(x = "PC1", y = "PC2", color = intgroup, label = intgroup),
-) +
-  geom_point(size = 5) +
-  scale_x_continuous(
-    name = paste0("PC1: ", percentVar[1], "% variance"),
-    limits = c(-20, 20)
-  ) +
-  scale_y_continuous(
-    name = paste0("PC2: ", percentVar[2], "% variance"),
-    limits = c(-20, 20)
-  ) +
-  coord_fixed() +
-  ggforce::geom_mark_ellipse(aes_string(label = "NULL", color = intgroup)) +
-  geom_text_repel(
-    size = 7,
-    vjust = "inward",
-    hjust = "inward",
-    show.legend = FALSE,
-    point.padding = 10
-  ) +
-  ggtitle("PCA") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(
-    axis.text = element_text(size = 20),
-    axis.title = element_text(size = 25),
-    title = element_text(size = 25, face = "bold"),
-    legend.text = element_text(size = 20)
-  )
-
-ggsave('results/basic_PCA.jpg', plot = PCA, width = 15, height = 15)
-
-# GSEAs -----------
-
-# Ensure that the everything is in MGI
-
-# Generalize to produce a GSEA for each res_list entry
-
-# In:
-# - res_list, res_list names
-# - ontology type, default to BP
-# Out:
-# - ontology rds
-# - ontology csv
-# - dotplot
-
 gsea_util <- function(result, name, ont = "BP", x = 1) {
   tmp <- as.data.frame(result)
   tmp <- tmp |>
     arrange(desc(log2FoldChange))
-  
+
   list <- tmp$log2FoldChange
   names(list) <- rownames(tmp)
-  
+
   bp_param <- SnowParam(
     workers = x,
     type = "SOCK",
     RNGseed = 42,
   )
-  
+
   gse <- gseGO(
     geneList = list,
     ont = ont,
@@ -320,11 +222,11 @@ gsea_util <- function(result, name, ont = "BP", x = 1) {
     by = "fgsea",
     BPPARAM = bp_param
   )
-  
+
   saveRDS(gse, str_glue("./results/GSEA/{name}.rds"))
-  
+
   write.csv(gse@result, str_glue("./Results/GSEA/{name}.csv"))
-  
+
   png(
     str_glue("Results/GSEA/dotplot_{name}.png"),
     width = 800,
@@ -336,4 +238,245 @@ gsea_util <- function(result, name, ont = "BP", x = 1) {
   dev.off()
 }
 
-walk2(res_list, results_dds_names, ~ gsea_util(.x, .y, x = 12))
+ora_util <- function(dat, map, cut = 1) {
+  degs_up <- dat |>
+    dplyr::filter(log2FoldChange >= cut)
+
+  degs_down <- dat |>
+    dplyr::filter(log2FoldChange <= -cut)
+
+  ora_up <- enrichGO(
+    gene = rownames(degs_up),
+    universe = map,
+    OrgDb = org.Mm.eg.db,
+    ont = "BP",
+    keyType = "SYMBOL",
+    readable = TRUE,
+    pvalueCutoff = 0.05,
+    pAdjustMethod = "BH"
+  )
+
+  ora_down <- enrichGO(
+    gene = rownames(degs_down),
+    universe = map,
+    OrgDb = org.Mm.eg.db,
+    ont = "BP",
+    keyType = "SYMBOL",
+    readable = TRUE,
+    pvalueCutoff = 0.05,
+    pAdjustMethod = "BH"
+  )
+
+  out <- list("up" = ora_up, "down" = ora_down)
+  return(out)
+}
+
+ORA_bar <- function(ora, top = 20, color = "sandybrown") {
+  # returns a bar plot of an ORA
+
+  tmp <- as.data.frame(ora@result) %>%
+    mutate(
+      neg_log10_padj = -log10(p.adjust),
+      Description = forcats::fct_reorder(Description, neg_log10_padj) # Order by significance
+    ) %>%
+    arrange(desc(neg_log10_padj)) %>%
+    head(top)
+
+  p <- ggplot(tmp, aes(x = neg_log10_padj, y = Description)) +
+    geom_col(fill = color) +
+    labs(
+      x = "-log10(Adjusted p-value)",
+      y = "GO Term",
+      title = "ORA: Enriched Biological Process Terms"
+    ) +
+    coord_cartesian(xlim = c(0, 20)) +
+    theme_minimal() +
+    theme(
+      axis.text.y = element_text(size = 10, face = "plain", color = "black"),
+      axis.text.x = element_text(size = 10, color = "black"),
+      axis.title.x = element_text(size = 12, margin = margin(t = 10)),
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.y = element_blank(),
+      panel.grid.major.x = element_line(linetype = "dotted", color = "grey"),
+      panel.grid.minor.x = element_blank(),
+      plot.margin = margin(1, 1, 1, 1, "cm")
+    )
+
+  return(p)
+}
+
+call_plot <- function(ora, name) {
+  for (i in 1:2) {
+    ORA_bar(ora$up)
+    ggsave(str_glue("./results/oras/{name}_ora_up.png"), bg = "white")
+
+    ORA_bar(ora$down)
+    ggsave(str_glue("./results/oras/{name}_ora_down.png"), bg = "white")
+  }
+}
+
+convert_export_ora <- function(ora_list) {
+  out <- list()
+
+  for (i in 1:length(ora_list)) {
+    new <- list("up" = c(), "down" = c())
+    new$up <- as.data.frame(ora_list[[i]]$up@result)
+    new$down <- as.data.frame(ora_list[[i]]$down@result)
+    out[[i]] <- new
+  }
+
+  names(out) <- names(ora_list)
+  return(out)
+}
+
+for (dds in list_dds) {
+  # Extracting results ----
+
+  results_dds_names <- resultsNames(dds)[-(resultsNames(dds) == "Intercept")]
+
+  res_list <- map(results_dds_names, ~ results(dds, name = .x))
+
+  names(res_list) <- results_dds_names
+
+  genes_annot <- dat |>
+    dplyr::select(external_gene_name, description) |>
+    distinct(external_gene_name, .keep_all = TRUE)
+
+  genes_annot <- as.data.frame(genes_annot)
+
+  rownames(genes_annot) <- genes_annot$external_gene_name
+  genes_annot <- genes_annot[, -1, drop = FALSE]
+
+  # raw
+  paths <- paste0("./results/data/", results_dds_names, ".csv")
+  tmp <- map(
+    res_list,
+    ~ merge(as.data.frame(.x), genes_annot, by = 'row.names', all = FALSE)
+  )
+  walk2(tmp, paths, write.csv)
+
+  #filtered
+  res_list_filtered <- map(res_list, ~ .x[.x$padj < 0.05 & !is.na(.x$padj), ])
+  tmp <- map(
+    res_list_filtered,
+    ~ merge(as.data.frame(.x), genes_annot, by = 'row.names', all = FALSE)
+  )
+
+  paths_filtered <- paste0(
+    "./results/data/",
+    results_dds_names,
+    "_filtered_padj05.csv"
+  )
+
+  walk2(tmp, paths_filtered, write.csv)
+
+  # summary of filtered
+
+  walk2(res_list_filtered, results_dds_names, summarize_dds_res)
+
+  # Making the results into filtered, ordered, and annotated dataframes
+  cutoff <- 1
+  res_workable <- list()
+  for (i in 1:length(res_list_filtered)) {
+    tmp <- as.data.frame(res_list_filtered[[i]])
+    tmp <- merge(tmp, genes_annot, by = 'row.names', all = FALSE)
+
+    tmp <- tmp |>
+      mutate(
+        dir = case_when(
+          log2FoldChange > cutoff ~ "UP",
+          log2FoldChange < -1 * cutoff ~ "DOWN",
+          T ~ "NS"
+        )
+      ) |>
+      arrange(desc(log2FoldChange))
+
+    res_workable[[i]] <- tmp
+  }
+
+  names(res_workable) <- results_dds_names
+
+  #Volcano Plots:
+
+  volcano_annot <- genes_annot[, -2, drop = FALSE]
+
+  walk2(res_list, results_dds_names, ~ volcano_dds(.x, .y, volcano_annot))
+
+  # Clustering -------
+
+  # for PCAs we need normalized counts and not LFC
+  rld_all <- rlog(dds, blind = FALSE)
+  rld_all_df <- as.data.frame(assay(rld_all))
+
+  intgroup = "treatment"
+
+  PCA_plot_all <- DESeq2::plotPCA(
+    rld_all,
+    intgroup = intgroup,
+    returnData = TRUE,
+    ntop = 1000
+  )
+  percentVar <- round(100 * attr(PCA_plot_all, "percentVar"))
+
+  PCA <- ggplot(
+    PCA_plot_all,
+    aes_string(x = "PC1", y = "PC2", color = intgroup, label = intgroup),
+  ) +
+    geom_point(size = 5) +
+    scale_x_continuous(
+      name = paste0("PC1: ", percentVar[1], "% variance"),
+      limits = c(-20, 20)
+    ) +
+    scale_y_continuous(
+      name = paste0("PC2: ", percentVar[2], "% variance"),
+      limits = c(-20, 20)
+    ) +
+    coord_fixed() +
+    ggforce::geom_mark_ellipse(aes_string(label = "NULL", color = intgroup)) +
+    geom_text_repel(
+      size = 7,
+      vjust = "inward",
+      hjust = "inward",
+      show.legend = FALSE,
+      point.padding = 10
+    ) +
+    ggtitle("PCA") +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(
+      axis.text = element_text(size = 20),
+      axis.title = element_text(size = 25),
+      title = element_text(size = 25, face = "bold"),
+      legend.text = element_text(size = 20)
+    )
+
+  ggsave('results/basic_PCA.jpg', plot = PCA, width = 15, height = 15)
+
+  # GSEAs -----------
+
+  walk2(res_list, results_dds_names, ~ gsea_util(.x, .y, x = 12))
+
+  # ORAs -------
+
+  map <- rownames(counts)
+  ora_list <- map(res_list, ~ ora_util(as.data.frame(.x), map))
+
+  paths_oras <- paste0(
+    "./results/oras/",
+    results_dds_names,
+    "_ora.xlsx"
+  )
+
+  # write_xlsx takes care of the writing both the up and down ora data to one
+  # excel file one per each sheet
+  # ahhhh but the issue is that I need the ora in full for the plot and then I
+  # need the ora@result to write as an excel file
+
+  ora_list_export <- convert_export_ora(ora_list)
+  walk2(ora_list_export, paths_oras, write_xlsx)
+
+  # then save the bar graphs for each ora
+
+  walk2(ora_list, results_dds_names, call_plot)
+}
+
+# finally have to consider extra comparisons if needed
